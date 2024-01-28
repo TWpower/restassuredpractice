25eecb53815d9a2741d3f620ff29f05ddeb2b45a
TWpower <elld8850@naver.com>
2024-01-28 22:59:24 +0900

    add more

--- src/test/java/me/twpower/restassuredpractice/RestAssuredPracticeTest.java
+++ src/test/java/me/twpower/restassuredpractice/RestAssuredPracticeTest.java
@@ -1,6 +1,9 @@
 package me.twpower.restassuredpractice;
 
 import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -20,7 +23,8 @@ public class RestAssuredPracticeTest {
     @Test
     void restAssuredPracticeTest() {
         // JSON Example
-        // Call http://echo.jsontest.com/key1/value1/key2/value2/key3/3
+        // Request Method: GET
+        // Call http://echo.jsontest.com/key1/value1/key2/value2/key3/3?queryParameterKey=queryParameterValue
         /*
         {
             "key1": "value1",
@@ -34,11 +38,18 @@ public class RestAssuredPracticeTest {
         // then(): Returns a validatable response that's lets you validate the response.
 
         // given() and when() returns RequestSpecification object
-        given().
-                header("Content-Type", "application/json").
+        ExtractableResponse<Response> extractableResponse = given().log().all().
+            header("Content-Type", "application/json"). // Specify the headers that'll be sent with the request.
+            pathParam("pathParameter", 3). // Specify a path parameter.
+            queryParam("queryParameterKey", "queryParameterValue"). // Specify a query parameter that'll be sent with the request.
+            //body(). // Specify request body.
         when().
-                get("/key1/value1/key2/value2/key3/3").
+            get("/key1/value1/key2/value2/key3/{pathParameter}").
         then().
-                body("key1", equalTo("value1"));
+            body("key1", equalTo("value1")).
+            extract();
+
+        Assertions.assertEquals(200, extractableResponse.statusCode());
+        Assertions.assertEquals("value1", extractableResponse.jsonPath().getString("key1"));
     }
 }
